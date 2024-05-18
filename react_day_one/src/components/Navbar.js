3cd901834943ee70349a6143a2aa17aadac5ced8
Eyram Tsagli <afiivy@gmail.com>
2024-05-17 22:23:57 -0500

    first commit for react

---
+++ react_day_one/src/components/Navbar.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function Navbar() {
+    return (
+        <div className='container' style= {{textAlign:'center', backgroundColor:'gray'}}>Navbar</div>
+    )    
+}
